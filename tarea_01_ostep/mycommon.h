43a7fa012548e736bc1d7077d2d9ad9159dec0ba
juandavidsandoval <50960608+juandavidsandoval@users.noreply.github.com>
2019-07-25 13:12:00 -0500

    ADELANTOS DEL PARCIAL

--- tarea_01_ostep/mycommon.h
+++ tarea_01_ostep/mycommon.h
@@ -3,11 +3,90 @@
  
 #include "pthread.h"
 #include "common.h"
+
+volatile int counter = 0; 
+int loops;
+pthread_mutex_t puerta;
+void *crear_puerta(){
+        pthread_mutex_init(&puerta,NULL);
+}
+void *cerrar_puerta (){
+        pthread_mutex_lock(&puerta);
+}
+void *abrir_puerta (){
+        pthread_mutex_unlock(&puerta);
+}
+void *destruir_puerta (){
+        pthread_mutex_destroy(&puerta);
+}
+void *worker(void *arg) {
+    cerrar_puerta();
+    int i;
+    
+    
+    for (i = 0; i < loops; i++) {
+
+        counter++;
+    }
+    
+    abrir_puerta();
+    return NULL;
+}
+
+int main(int argc, char *argv[]) {
+    clock_t time1, time2;
+    double secs;
+    time1 = clock();
+    time2 =clock();
+
+    secs = (double)(time2 - time1) / CLOCKS_PER_SEC;
+
+    printf(" el tiempo es: %f\n", secs);
+     if (argc != 2) { 
+        fprintf(stderr, "usage: threads <loops>\n"); 
+
+    
+
+        exit(1); 
+  
+    return 0;
+
+
+    }
+    crear_puerta();
+
+    loops = atoi(argv[1]);
+    pthread_t p1, p2;
+    printf("Initial value : %d\n", counter);
+    Pthread_create(&p1, NULL, worker, NULL);
+    Pthread_create(&p2, NULL, worker, NULL);
+    
+    Pthread_join(p1, NULL);
+    Pthread_join(p2, NULL);
+    printf("Final value   : %d\n", counter);
+    return 0;
+    
+    
+
+    destruir_puerta();
+    
+
+    
+
+
+}
+
+
+
+
+
 #define puerta pthread_mutex_t // nuevo tipo de dato
 #define crear_puerta(m) pthread_mutex_init(&m,NULL) // funcion para crear puerta
 #define cerrar_puerta(m) pthread_mutex_lock(&m) // funcion para cerrar la puerta
 #define abrir_puerta(m) pthread_mutex_unlock(&m) //funcion para abrir la puerta
 #define destruir_puerta(m) pthread_mutex_destroy(&m) // funcion para destruir puerta
+
+
 
 #endif
  	
--- tarea_01_ostep/threads.c
+++ tarea_01_ostep/threads.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "common.h"
 #include "common_threads.h"
+#include <time.h>
+#include <pthread.h>
 
 volatile int counter = 0; 
 int loops;
@@ -21,28 +23,65 @@ void *destruir_puerta (){
 void *worker(void *arg) {
     cerrar_puerta();
     int i;
+    
+    
     for (i = 0; i < loops; i++) {
+	
 	counter++;
     }
+    
     abrir_puerta();
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
+    clock_t time1, time2;
+    double secs;
+    time1 = clock();
+    time2 =clock();
+
+    secs = (double)(time2 - time1) / CLOCKS_PER_SEC;
+
+    
+
+    
+
+    printf(" el tiempo es: %f\n", secs);
+    
+ 
+    
+
+
     if (argc != 2) { 
 	fprintf(stderr, "usage: threads <loops>\n"); 
+	
+    
+	
 	exit(1); 
-    } 
+  
+    return 0;
+
+
+    }
+    crear_puerta();
+     
     loops = atoi(argv[1]);
     pthread_t p1, p2;
     printf("Initial value : %d\n", counter);
     Pthread_create(&p1, NULL, worker, NULL);
     Pthread_create(&p2, NULL, worker, NULL);
-    crear_puerta();
+    
     Pthread_join(p1, NULL);
     Pthread_join(p2, NULL);
     printf("Final value   : %d\n", counter);
     return 0;
-    destruir_puerta();
-}
+    
+    
 
+    destruir_puerta();
+    
+
+    
+
+
+}
